7334ed936942a0d00bf4f9cd681425f124293f85
Liaw MZ <liaw@primalux.com.sg>
2022-03-30 17:10:25 +0800

    Init.

---
+++ Entities/AppObject.cs
@@ -0,0 +1,11 @@
+namespace Entities
+{
+    public class AppObject
+    {
+        public int Id { get; set; }
+        public string ObjType { get; set; }
+        public string ObjName { get; set; }
+        public string ObjDesc { get; set; }
+        public string ObjText { get; set; }
+    }
+}
